6d7658d826babccdeb6a0a6a8e90cbd7470121ec
Akhilsoe <19cs011akhi@ug.cusat.ac.in>
2022-08-13 16:04:43 +0530

    commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between p-4">
+      <div className="text-red-600 font-bold text-3xl"> NETFLIX</div>
+      <div className="">
+        <button className="text-white cursor-pointer pr-4"> Sign In</button>
+        <button className="bg-red-600 px-6 py-2 text-white rounded cursor-pointer">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
