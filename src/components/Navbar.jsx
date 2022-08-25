50422f50fcc7b1f49d1c73dc6baae5a32f434df9
Akhilsoe <19cs011akhi@ug.cusat.ac.in>
2022-08-26 00:01:42 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,9 @@ const Navbar = () => {
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
       <div className="text-red-600 font-bold text-3xl"> NETFLIX</div>
       <div className="">
-        <button className="text-white cursor-pointer pr-4"> Sign In</button>
+        <button className="text-white cursor-pointer py-2 px-6 mr-3">
+          Sign In
+        </button>
         <button className="bg-red-600 px-6 py-2 text-white rounded cursor-pointer">
           Sign Up
         </button>
