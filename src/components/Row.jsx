d21583a8a60a4f44eb8ab46819410dd1ae84e275
Akhilsoe <19cs011akhi@ug.cusat.ac.in>
2022-08-26 02:44:16 +0530

    final ui

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Movie from "./Movie";
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -13,18 +13,25 @@ const Row = ({ title, fetchURL }) => {
     });
   }, [fetchURL]);
 
-  const slideLeft=[]
+  const slider = useRef();
+  const slideLeft = () => {
+    slider.current.scrollLeft = slider.current.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    slider.current.scrollLeft = slider.current.scrollLeft + 500;
+  };
 
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4 md:ml-3">{title}</h2>
       <div className="relative flex items-center group">
         <MdChevronLeft
+          onClick={slideLeft}
           className="bg-white left-0 rounded-full absolute z-10 opacity-50 hover:opacity-100  cursor-pointer hidden group-hover:block"
           size={40}
         />
         <div
-          id={"slider"}
+          ref={slider}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative "
         >
           {movies.map((item, id) => (
@@ -32,6 +39,7 @@ const Row = ({ title, fetchURL }) => {
           ))}
         </div>
         <MdChevronRight
+          onClick={slideRight}
           className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100  cursor-pointer hidden group-hover:block"
           size={40}
         />
